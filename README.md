6407361c0c2f3d0714fdafc13893b24d5ee671be
Glitch (array-transversals) <none>
2020-07-12 00:36:29 +0000

    🏅🏄 Updated with Glitch

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# array-iteration
+ARRAY ITERATION
+=================
+
+Iterate literally means 'to perform repeatedly'. While human beings get bored of doing something repeatedly, machines excel at it.
+
+![](https://media.giphy.com/media/J342sjcgjiqRLoFnQS/giphy.gif)
+
+To "iterate over a list" (or an array in JavaScript) means to perform the same task over and over again, once for each item in the list.
+
+To iterate over an array, we use a for loop in additon with the `.length` propery for JavaScript arrays. An example transveral can be seen below.
+```javascript
+var myList = ["item 1", "item 2", "item 3", "item 4", "item 5", "item 6"]
+for(var i = 0; i < myList.length; i++){
+  console.log(myList[i])
+}
+```
+The for loop iterates for the entire length of the list. The value of i (which can be any word you want) is the position in the list where you are at. So the console will log each item in the list for the example shown above
+
+TASK
+------------
+To complete today's task, open the script.js file and follow the directions to print various statements to the console.
---
+++ script.js
@@ -0,0 +1,90 @@
+//1. Make a list called favoriteFoods and store at least six of your favorite foods in the list
+
+
+//2. Print out each of your favorite foods in the console saying "My favorite food is ________"
+
+
+
+
+// 3. Let's expand the dialogue - For each food in the list, print out three lines.
+//     "Have you ever tried ___?"
+//     "I really recommend that you try _____."
+//     "_____ is delicious."
+
+
+
+
+
+
+//  4a. Now for each food in that list, let's be specific about how MUCH you likes them each We'll do this without iteration first.
+//  For example, print out "Steven's #1 favorite food is Sushi" and then "Steven's #2 favorite food is Pizza" and so on until you've made an ordered list.
+// To start, you can copy-paste this line as many times as you need, and make small adjustments:
+//
+//  console.log(`Steven's #1 favorite food is ${favoriteFoods[0]}`)
+
+
+
+
+// # 4b. Now comment out the above code, and try doing the same thing with a loop - you will likely need fewer lines of code.
+// # This can be done a number of different ways, but the easiest is probably by iterating over each index in the favoriteFoods and modifying i to represent the value you are currently at. Explore documentation for this!
+
+
+
+
+
+// # 5. Below is a massive list of numbers. Iterate over the list and print out only the numbers less than 100. Put all these in a lessThanOneHundred list.
+var lessThanOneHundred = [];
+var manyNumbers = [109, 141, 44, 51, 133, 366, 339, 248, 226, 321, 97, 195, 245, 252, 238, 1, 366, 47, 189, 91, 148, 88, 194, 106, 5, 128, 165, 337, 380, 181, 143, 95];
+
+
+
+
+// # 6. Now iterate over the list but print out only the even ones. Put all of these in an evenNumbers list
+var evenNumbers = []
+
+
+
+
+// # 7. Below is a list of 99 names. Iterate over them all and print out only the names that include the letter "a". Store these names in a list called namesWithA
+var manyNames = ["Alexa","Burke","Kasimir","Baxter","Carissa","Vielka","Derek","Jemima","Jackson","Keegan","Graham","Melissa","Jeanette","Grant","Kirsten","Naida","Brody","Ishmael","Kane","Seth","Rae","Eagan","Camille","Alana","Vance","Melinda","Tarik","Risa","Jordan","Camilla","Karly","Baker","Adena","Calvin","Kendall","Nasim","Kellie","Dana","Rhoda","Linus","Tyler","Ahmed","Dante","Shay","Lael","Tana","Claudia","Chadwick","Tara","Fulton","Justine","Malcolm","Rowan","Christopher","Ciaran","Ivan","Hiram","Blake","Colton","Nathaniel","Moses","Cynthia","George","Ignacia","Chanda","Wyatt","Amethyst","Vladimir","Adam","Boris","Joseph","Scarlett","Kieran","Curran","Dalton","Paul","Phillip","Plato","Renee","Natalie","Barbara","Keiko","Oleg","Xerxes","Caesar","Kareem","Ahmed","Charles","Cyrus","Adria","Winifred","Pandora","Wynne","Simon","Wanda","Coby","Nolan","Marsden","Courtney"]
+
+
+
+
+// 8. Now iterate over the list and print out only the names which have more than 6 letters in them. Store these names in a list called longNames
+
+
+
+
+// 9. Iterate over the list and find the longest name in the whole thing. 
+// Then print out "The longest name in the list is ______. It has ___ letters in it."
+
+
+
+
+
+// 10. Make a second list called shortNames that contains all names that are 6 letters or shorter
+
+
+
+
+
+
+// 11. Print out the longNames and shortNames lists.
+
+
+
+
+// 12. Are there more long names or short names? Write code to figure that out.
+
+
+
+
+
+// CHALLENGE: Iterate over the list of numbers to find the average of them all.
+
+
+
+
+
+//DOUBLE TRIPPLE BONUS CHALLENGE: Find all duplicated names on the list and return the number of times each name is duplicated in the manyNames list.
